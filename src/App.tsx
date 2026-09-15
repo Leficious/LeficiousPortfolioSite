3fb625bf0950d14aa3d934792cfb7a5c24a2abfb
Leficious <118146444+Leficious@users.noreply.github.com>
2026-09-15 04:34:27 -0700

    Enable interactive ambient desktop backdrop

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import { Route, Routes, useLocation } from "react-router-dom";
+import { AmbientBackdrop } from "./components/AmbientBackdrop";
 import { AboutPage } from "./pages/AboutPage";
 import { HomePage } from "./pages/HomePage";
 import { GalleryPage } from "./pages/GalleryPage";
@@ -14,12 +15,17 @@ export function App() {
   }, [pathname]);
 
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/gallery" element={<GalleryPage />} />
-      <Route path="/about" element={<AboutPage />} />
-      <Route path="/projects/:slug" element={<ProjectPage />} />
-      <Route path="*" element={<NotFoundPage />} />
-    </Routes>
+    <div className="relative min-h-screen">
+      <AmbientBackdrop />
+      <div className="relative z-10">
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/gallery" element={<GalleryPage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects/:slug" element={<ProjectPage />} />
+          <Route path="*" element={<NotFoundPage />} />
+        </Routes>
+      </div>
+    </div>
   );
 }
